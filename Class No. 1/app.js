ac38734bba3ae6d4a5e0cc58d013dd8738845e95
mali521 <decent_mughal007@hotmail.com>
2016-09-06 17:49:08 +0500

    Class No. 1

---
+++ Class No. 1/app.js
@@ -0,0 +1,5 @@
+var name , name1; 
+name = "Muhammad Ali";
+name1 = "Atique";
+alert(name1);
+alert(name);
---
+++ Class No. 1/app1.js
@@ -0,0 +1,7 @@
+var a = 5;
+var b = "10";
+var c = 6+7*4/2 ;
+var d = 10 % 3;
+alert(c);
+alert(d);
+console.log(100);
