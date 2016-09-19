af73b2199429aa0b65776fcfe3faaf13f663b475
mali521 <decent_mughal007@hotmail.com>
2016-09-19 18:19:35 +0500

    Class No. 3

---
+++ Class No. 3 (18-9-16)/Assignment.js
@@ -0,0 +1,61 @@
+//var a = prompt ("Enter string","Your string");
+//var b = prompt ("Enter string","Your string");
+//var c = a + b ;
+//alert(c);
+
+
+//var x = prompt ("Enter Number","Your number");
+//var y = prompt ("Enter Number","Your Number");
+//var z = parseInt(x) + parseInt(y) ;
+//alert(z);
+
+//Arithmatic Operators
+// + , - , / , % , *
+
+//Comparison Operators:
+// < , > , <= , >= , === , !==
+
+//Logical Operators:
+// && , ||
+
+
+//var a = prompt ("Enter your Name");
+//if ( a === "Ahmed"){
+//	console.log("True");
+//}
+//else{
+//	console.log("False");
+//}
+
+
+var x = prompt("Enter your Percentage", "Your Percentage");
+if(x <= 100 && x >= 90){
+	alert("A+");
+}
+else if (x < 90 && x >=80){
+	alert("A");
+}
+else if (x < 80 && x >=70){
+	alert("B");
+}
+else if (x < 70 && x >=60){
+	alert("C");
+}
+else if (x < 60 && x >=50){
+	alert("D");
+}
+else if (x < 50 && x >=40){
+	alert("E");
+}
+else {
+	alert("F");
+}
+
+
+
+if ((SAT > avg && GPA > 2.5) || sport === "football") { 
+	alert("Welcome to Bubba State!"); 
+	 } 
+	 else { 
+		   alert("Have you looked into appliance repair?");
+		    }
