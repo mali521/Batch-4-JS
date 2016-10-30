d758a5d59aa608340a7d4ae7e16cbc030af1232f
mali521 <decent_mughal007@hotmail.com>
2016-10-30 10:54:38 +0500

    Code Uploaded

---
+++ Class No. 9 (30-10-2016)/app.js
@@ -0,0 +1,56 @@
+//var date = new Date();
+//alert(date);
+//var Instring = date.toString();
+//var date1 = Instring.slice(8,10);
+//alert(date1);
+
+//var date2 = date.getTime(); 
+//alert(date2);
+
+// var currentTime = new Date();
+// var nextTime = new Date("Jan 20, 2025");
+// var time1 = currentTime.getTime();
+// var time2 = nextTime.getTime();
+// var Diff = time2 - time1;
+// var TofindDay = Diff / (1000*60*60*24);
+// var finalValue = Math.floor(TofindDay);
+// alert(finalValue);
+
+//  var d = new Date(); 
+//  d.setFullYear(2001);
+//  alert(d);
+
+
+//General Syntax:
+
+// function func(){
+//     alert("My first function")
+// }
+
+// func();
+
+// var func2 = function(){
+// alert("My first function")
+// }
+
+// func2();
+
+
+
+// function greet(){
+//     alert("Hello World");
+// }
+
+// greet();
+
+
+// function greet(greetings){
+//     alert(greetings);
+// }
+// greet("Hello World");
+
+
+function Addition(a,b,c){
+alert(a+b+c);
+}
+Addition(2,3,4);
